4b7b5e07ba8b1707e97f9e063b881634002504e0
tetoabdallah-rgb <tetoabdallah@gmail.com>
2026-08-08 00:28:06 +0300

    Update sw.js - Inject PDF capabilities & bump SW

--- sw.js
+++ sw.js
@@ -1,48 +1,18 @@
-const CACHE_NAME = 'promoter-cache-v24';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
-];
-
-self.addEventListener('install', event => {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener('install', (e) => {
+    self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                return caches.delete(key);
+            }));
         })
-      );
-    }).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request).then(response => {
-      // Don't cache if not a valid response
-      if (!response || response.status !== 200 || response.type !== 'basic') {
-        return response;
-      }
-      // Clone response and cache it
-      let responseToCache = response.clone();
-      caches.open(CACHE_NAME).then(cache => {
-        cache.put(event.request, responseToCache);
-      });
-      return response;
-    }).catch(() => {
-      return caches.match(event.request);
-    })
-  );
+    );
+    self.registration.unregister().then(() => {
+        self.clients.matchAll().then((clients) => {
+            clients.forEach(client => client.navigate(client.url));
+        });
+    });
 });
